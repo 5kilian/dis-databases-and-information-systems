6a02ddfe866b53f4bbae00c9599fff3ae964b1f5
5kilian <5kilian@informatik.uni-hamburg.de>
2018-05-07 22:03:06 +0200

    Add secret window

---
+++ src/de/estate/manager/controller/SecretController.java
@@ -0,0 +1,9 @@
+package de.estate.manager.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SecretController {
+    @FXML
+    public TextField secretField;
+}
